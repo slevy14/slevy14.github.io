04aa9d618a3017425ac8048ba13e73725dd7f50c
slevy14 <sammyglevy@gmail.com>
2023-09-10 19:17:46 -0700

    added item and npc placeholders for all temp rooms

--- js/rooms.js
+++ js/rooms.js
@@ -19,6 +19,12 @@ var rooms = {
         "description" : "You approach the weathered wooden shack. Some of the wood has begun to rot, but it seems functional. The rusty doorknob has a keyhole, but it seems to be <b>locked</b> The rest of the beach is to the <b>south</b>.",
         "directions"  : {
             "south" : "start"
+        },
+        "items" : {
+            "itemA" : "placeholder"
+        },
+        "npcs" : {
+            "npcA" : "placeholder"
         }
     },
     "cliff" : {
@@ -26,6 +32,12 @@ var rooms = {
         "directions"  : {
             "north" : "start",
             "west"  : "ocean"
+        },
+        "items" : {
+            "itemA" : "placeholder"
+        },
+        "npcs" : {
+            "npcA" : "placeholder"
         }
     },
     "ocean" : {
@@ -33,6 +45,12 @@ var rooms = {
         "directions"  : {
             "east"  : "cliff",
             "north" : "start",
+        },
+        "items" : {
+            "itemA" : "placeholder"
+        },
+        "npcs" : {
+            "npcA" : "placeholder"
         }
     },
 
@@ -53,32 +71,32 @@ var rooms = {
 
 
 
-var old_rooms = {
-    "start" : {
-        "description" : "You are on a beach. You see a wooden shack to the <b>north</b> and a cliff to the <b>south</b>.",
-        "directions"  : {
-            "north" : "shack-outside",
-            "south" : "cliff"        
-        }
-    },
-    "shack-outside" : {
-        "description" : "You approach the weathered wooden shack. Some of the wood has begun to rot, but it seems functional. The rusty doorknob has a keyhole, but it seems to be <b>locked</b> The rest of the beach is to the <b>south</b>.",
-        "directions"  : {
-            "south" : "start"
-        }
-    },
-    "cliff" : {
-        "description" : "You arrive at a towering rocky cliff, with seemingly no way to reach the top. To the <b>north</b> is the rest of the beach, and to the <b>west</b> is the ocean",
-        "directions"  : {
-            "north" : "start",
-            "west"  : "ocean"
-        }
-    },
-    "ocean" : {
-        "description" : "You stare out at the ocean. It <b>stares</b> back at you. To the <b>east</b> are the cliffs, and to the <b>north</b> is the rest of the beach.",
-        "directions"  : {
-            "east"  : "cliff",
-            "north" : "start",
-        }
-    }
-}
+// var old_rooms = {
+//     "start" : {
+//         "description" : "You are on a beach. You see a wooden shack to the <b>north</b> and a cliff to the <b>south</b>.",
+//         "directions"  : {
+//             "north" : "shack-outside",
+//             "south" : "cliff"        
+//         }
+//     },
+//     "shack-outside" : {
+//         "description" : "You approach the weathered wooden shack. Some of the wood has begun to rot, but it seems functional. The rusty doorknob has a keyhole, but it seems to be <b>locked</b> The rest of the beach is to the <b>south</b>.",
+//         "directions"  : {
+//             "south" : "start"
+//         }
+//     },
+//     "cliff" : {
+//         "description" : "You arrive at a towering rocky cliff, with seemingly no way to reach the top. To the <b>north</b> is the rest of the beach, and to the <b>west</b> is the ocean",
+//         "directions"  : {
+//             "north" : "start",
+//             "west"  : "ocean"
+//         }
+//     },
+//     "ocean" : {
+//         "description" : "You stare out at the ocean. It <b>stares</b> back at you. To the <b>east</b> are the cliffs, and to the <b>north</b> is the rest of the beach.",
+//         "directions"  : {
+//             "east"  : "cliff",
+//             "north" : "start",
+//         }
+//     }
+// }
